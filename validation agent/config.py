c45f4910b84390d31b4b98f7e7a0409d601d3c24
Surender0101 <slakshmislakshmi50@gmail.com>
2026-01-29 12:06:17 +0530

    Delete validation agent directory

--- validation agent/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-RAPIDAPI_HOST = os.getenv("RAPIDAPI_HOST")
-
-GST_VERIFY_URL = "https://gst-verification.p.rapidapi.com/v3/tasks/sync/verify_with_source/ind_gst_certificate"
